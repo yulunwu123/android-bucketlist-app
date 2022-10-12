a08e1cce862aad37928a5b0fe3a6cead01d3bf54
yulunwu123 <yw9pz@virginia.edu>
2022-10-12 16:59:44 -0400

    try commiting

---
+++ app/src/main/java/edu/virginia/cs4720/bucketlist/yw9pz/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.virginia.cs4720.bucketlist.yw9pz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
